b5cb860e8b1d2072aac98e61a1e37b6f2b144291
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-05-16 13:47:25 +0700

    feat: get all users api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const app = express();
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize('kata_pokemonfo', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
@@ -18,10 +18,22 @@ const User = sequelize.define('User', {
 });
 
 // Define a route to handle POST requests for creating a user
-app.post('/users', async (req, res) => {
+app.post('/api/users', async (req, res) => {
   try {
     // Extract data from the request body
     const { name } = req.body;
+
+    // Check if the name is not empty
+    if (!name) {
+      return res.status(400).json({ error: 'Name cannot be empty' });
+    }
+
+    // Check if the user already exists
+    if (await User.findOne({ where: { name } })) {
+      // Update the existing user with the new name
+      await User.update({ name }, { where: { name } });
+      return res.json({ message: 'User updated' });
+    }
 
     // Create a new user in the database
     const newUser = await User.create({ name });
@@ -32,6 +44,24 @@ app.post('/users', async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Something went wrong' });
   }
+});
+
+// Define a route to handle GET requests for getting all users
+app.get('/api/users', async (req, res) => {
+  try {
+    // Get all users from the database
+    const users = await User.findAll();
+
+    // Return all users as the response
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+sequelize.sync().then(() => {
+  console.log('Database & tables created!');
 });
 
 // Start the server
